b3a75b6b5c19038a1d31dbda2fbc605cf208c93e
Andy <amgauthier1992@gmail.com>
2024-04-22 12:01:36 -0400

    adds additional earth maps

--- src/components/Planets/Earth/index.tsx
+++ src/components/Planets/Earth/index.tsx
@@ -3,23 +3,33 @@ import { useTexture } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { Mesh } from 'three';
 import EarthMap from '../../../assets/earth.jpg';
+import EarthNormalMap from '../../../assets/earth-normal-map.jpg';
+import EarthSpecularMap from '../../../assets/earth-specular-map.jpg';
+import EarthDisplacementMap from '../../../assets/earth-displacement-map.jpg';
 
 const Earth = () => {
   const earthRef = useRef<Mesh>();
-  const earthTexture = useTexture<string>(EarthMap);
+  const [earthTexture, earthNormalMap, earthSpecularMap, earthDisplacementMap] = useTexture<
+    string[]
+  >([EarthMap, EarthNormalMap, EarthSpecularMap, EarthDisplacementMap]);
 
   useFrame(() => {
     if (earthRef.current) {
-      earthRef.current.rotation.y += 0.001; // Increase rotation speed for y-axis
-      earthRef.current.rotation.x += 0.005; // Increase rotation speed for x-axis
-      earthRef.current.rotation.z += 0.005; // Increase rotation speed for z-axis
+      earthRef.current.rotation.y += 0.002;
     }
   });
 
   return (
     <mesh ref={earthRef as React.MutableRefObject<Mesh>}>
       <sphereGeometry args={[1, 32, 32]} />
-      <meshStandardMaterial map={earthTexture} />
+      <meshPhongMaterial
+        displacementMap={earthDisplacementMap}
+        displacementScale={0.05}
+        map={earthTexture}
+        normalMap={earthNormalMap}
+        shininess={10}
+        specularMap={earthSpecularMap}
+      />
     </mesh>
   );
 };
