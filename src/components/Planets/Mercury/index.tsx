e225fd458e2e6069c92e59f9192427a89b8b8614
Andy <amgauthier1992@gmail.com>
2024-04-23 11:59:52 -0400

    adds Mercury with relative scaling and orbit behavior

---
+++ src/components/Planets/Mercury/index.tsx
@@ -0,0 +1,37 @@
+import { useRef } from 'react';
+import { useTexture } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import { Mesh, Vector3 } from 'three';
+import MercuryMap from '/assets/mercury/mercury.jpg';
+
+const Mercury = () => {
+  const mercuryRef = useRef<Mesh>();
+  const mercuryPositionRef = useRef(new Vector3(36, 0, 0));
+
+  const [mercuryTexture] = useTexture<string[]>([MercuryMap]);
+
+  useFrame(({ clock }) => {
+    if (mercuryRef.current) {
+      const angle = clock.getElapsedTime() * 1.5;
+      const distance = 36;
+      const x = Math.sin(angle) * distance;
+      const z = Math.cos(angle) * distance;
+      mercuryRef.current.position.set(x, 0, z);
+      mercuryRef.current.rotation.y += 0.02;
+      mercuryPositionRef.current = mercuryRef.current.position;
+    }
+  });
+
+  return (
+    <mesh
+      //castShadow
+      receiveShadow
+      ref={mercuryRef as React.MutableRefObject<Mesh>}
+    >
+      <sphereGeometry args={[1.5, 32, 32]} />
+      <meshPhongMaterial map={mercuryTexture} />
+    </mesh>
+  );
+};
+
+export default Mercury;
--- src/layouts/SolarSystem.tsx
+++ src/layouts/SolarSystem.tsx
@@ -1,6 +1,13 @@
 import AnimatedStars from '../components/AnimatedStars';
 import Earth from '../components/Planets/Earth';
+// import Jupiter from '../components/Planets/Jupiter';
+// import Mars from '../components/Planets/Mars';
+import Mercury from '../components/Planets/Mercury';
+// import Neptune from '../components/Planets/Neptune';
+// import Saturn from '../components/Planets/Saturn';
 import Sun from '../components/Sun';
+// import Uranus from '../components/Planets/Uranus';
+// import Venus from '../components/Planets/Venus';
 
 const SolarSystem = () => {
   return (
@@ -11,7 +18,14 @@ const SolarSystem = () => {
       />
       <AnimatedStars />
       <Sun />
+      <Mercury />
+      {/* <Venus /> */}
       <Earth />
+      {/* <Mars />
+      <Jupiter />
+      <Saturn />
+      <Uranus />
+      <Neptune /> */}
     </>
   );
 };
