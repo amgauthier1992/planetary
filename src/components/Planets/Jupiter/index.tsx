bfa5276e90ae9a7fcf0f60fd84830c05439fd3f8
Andy <amgauthier1992@gmail.com>
2024-05-03 15:22:41 -0400

    converts Jupiter orbital period from years to days

--- src/components/Planets/Jupiter/index.tsx
+++ src/components/Planets/Jupiter/index.tsx
@@ -45,7 +45,7 @@ const Jupiter = () => {
         mass: '1.8982e27',
         moons: 79,
         name: 'Jupiter',
-        orbitalPeriod: 11.9,
+        orbitalPeriod: 4343.5,
         rotationalPeriod: 0.41,
         sunDistance: '778.6e6',
         surfaceTemp: -108,
